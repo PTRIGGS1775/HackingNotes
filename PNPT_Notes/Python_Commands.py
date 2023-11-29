96cd43edf8291a00df89df315765e633540d9d7c
kali <pt.riggs1775@gmail.com>
2023-11-29 18:31:25 -0500

    Restarting python

---
+++ PNPT_Notes/Python_Commands.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+#Print String
+print('##String Section')
+print("Hello, world!")
+print("""This string
+      runs multiple lines!""") #Triple quotes for lines
+print("This string" + "Concatenates lines") #Concatenating
+print('\n') #Prints new line
+print('Test new line out')
+
+
+
+#Math
+print('\n')
+print('##Math Section')
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #exponents
+print(50 % 6) #modulo - takes what is left over
+print(50 / 6) #division with decimals
+print(50 // 6) #no remainder
