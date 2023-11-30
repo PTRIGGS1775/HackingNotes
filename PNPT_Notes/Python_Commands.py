cfb6a3d03c410ecaa1816eca4369ce9520719299
kali <pt.riggs1775@gmail.com>
2023-11-30 18:07:11 -0500

    minor

--- PNPT_Notes/Python_Commands.py
+++ PNPT_Notes/Python_Commands.py
@@ -84,7 +84,31 @@ def nl(): #faster way going forward to print new lines.
 #Boolean Expressions
 nl()
 print('##Boolean Expressions Section') 
+bool1 = True
+bool2 = 3*3 == 9
+bool3 = False
+bool4 = 3*3 != 9
 
+print(bool1,bool2,bool3,bool4)
+print(type(bool1))
+
+bool5 = "True"
+print(type(bool5))
+
+nl()
+
+#Relational and Boolean operators
+greater_than = 7 > 5
+less_than = 5 < 7
+greater_than_equal_to = 7 >=7
+less_than_equal_to = 7 <= 7
+
+test_and = True and True #True
+test_and2 = True and False #False
+test_or = True or True #True
+test_or2 = True or False #True
+
+test_not = not True #False:
 
 
 
