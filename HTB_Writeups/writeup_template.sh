58870bcfada9e0816a6b74dc73d24681146e0da6
kali <pt.riggs1775@gmail.com>
2024-01-19 16:49:34 -0500

    Created directory for HTB Blue

--- HTB_Writeups/writeup_template.sh
+++ HTB_Writeups/writeup_template.sh
@@ -1,8 +1,12 @@
 #/bin/bash
+#
+#This script was written to save time when doing HTB writeups. 
+#Basically you're just copying the URL and the IP once you spawn your machine to get the initial portion of your work done for you.
 
 boxurl="$1"
 ip="$2"
 
+#Check to see that the script was run with the appropriate arguments.
 if [ -z $boxurl ] | [ -z $ip ]; then
 	echo "[*] Correct Usage: $0 https://app.hackthebox.com/machines/{box name} IP.IP.IP.IP"
 	exit 0
@@ -22,9 +26,11 @@ fi
 boxname=$(cut -d '/' -f 5- <<< $boxurl)
 todaydate=$(date '+%Y-%m-%d')
 
+#Make the writeup directories
 mkdir $boxname
 mkdir $boxname/images
 
+#I don't know a faster way to redirect input per line to a file so I added each echo sent to the Readme.md file ona separate line.
 echo "# $boxname" > $boxname/Readme.md
 echo "" >> $boxname/Readme.md
 echo "<h1 align="center">" >> $boxname/Readme.md
@@ -50,10 +56,6 @@ echo "\`\`\`bash" >> $boxname/Readme.md
 echo "sudo nmap -sC -sV $ip > $boxname/nmap_$ip" >> $boxname/Readme.md
 echo "\`\`\`" >> $boxname/Readme.md
 
-git add . 
-git commit -m "Created directory for HTB $boxname"
-git push
-
 printf "\n\n\n"
 printf "============================================================================================================="
 printf "\n\n"
@@ -61,5 +63,21 @@ printf "[*] The script has completed. You know have a $boxname directory with an
 read -p "Would you like to run your nmap script? (sudo nmap -sC -sV $ip > $boxname/nmap_$ip)? " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+	#Start the visual wait command.
+	while true;do echo -n .;sleep 1;done &
 	sudo nmap -sC -sV $ip > $boxname/nmap_$ip
+	#Complete the visual wait command and echo step.
+	kill $!; trap 'kill $!' SIGTERM
 fi
+
+#This will only work without a prompt within my current environment.
+#This will create issues if I do a git pull, I'll need to reset my git variables.
+git add . 
+git commit -m "Created directory for HTB $boxname"
+git push
+
+printf "\n\n\n"
+printf "============================================================================================================="
+printf "\n\n"
+print "Your NMAP scan is complete and all files have been added to your github"
+
