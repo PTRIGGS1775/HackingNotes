3781031206edff4ab543557e35c845f8dc4460cc
kali <pt.riggs1775@gmail.com>
2024-01-19 16:28:06 -0500

    Created directory for HTB Blue

--- HTB_Writeups/writeup_template.sh
+++ HTB_Writeups/writeup_template.sh
@@ -47,17 +47,19 @@ echo "# Nmap" >> $boxname/Readme.md
 echo "" >> $boxname/Readme.md
 echo "![](images/nmap.png)" >> $boxname/Readme.md
 echo "\`\`\`bash" >> $boxname/Readme.md
-echo "sudo nmap -sC -sV -oA -oA $boxname/nmap_$ip $ip" >> $boxname/Readme.md
+echo "sudo nmap -sC -sV -oA $ip > $boxname/nmap_$ip" >> $boxname/Readme.md
 echo "\`\`\`" >> $boxname/Readme.md
 
 git add . 
 git commit -m "Created directory for HTB $boxname"
 git push
 
-printf "\n\n\n\n" + 20*"=" + "\n"
+printf "\n\n\n\n"
+printf "============================================================================================================="
+printf "\n"
 printf "[*] The script has completed. You know have a $boxname directory with an images subdirectory.[*] You need to save your screenshot of the box as 'img.png' and the screenshot of your nmap scan as 'nmap.png' for all this hard work to be displayed.\n"
-read -p "Would you like to run your nmap script? (sudo nmap -sC -sV -oA -oA $boxname/nmap_$ip $ip)? " -n 1 -r
+read -p "Would you like to run your nmap script? (sudo nmap -sC -sV -oA $ip > $boxname/nmap_$ip)? " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	sudo nmap -sC -sV -oA -oA $boxname/nmap_$ip $ip
+	sudo nmap -sC -sV -oA $ip > $boxname/nmap_$ip
 fi
